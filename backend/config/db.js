36815646aa2c43c976b33734222c0247f7e6487a
shobhitsinha-25 <sinhashobhit025@gmail.com>
2025-02-03 19:22:32 +0530

    feat: commit with Docker support

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,10 +2,11 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 
 dotenv.config();
+const mongouri=process.env.MONGO_URI || "mongodb://localhost:27017/faq_db"
 
 const connectDB=async ()=>{
     try{
-        await mongoose.connect(process.env.MONGO_URI);
+        await mongoose.connect(mongouri);
         console.log("connected successfully with the Database");
     }
     catch(error)
